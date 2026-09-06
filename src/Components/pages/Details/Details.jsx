0fd225318069e7f657043acf3594ff602125de1c
manshika140899 <manshika140899@gmail.com>
2026-09-07 03:17:57 +0500

    Fix details page

--- src/Components/pages/Details/Details.jsx
+++ src/Components/pages/Details/Details.jsx
@@ -29,31 +29,47 @@
 
 
 
-import React from "react";
-import { useParams } from "react-router-dom";
+// import React from "react";
+// import { useParams } from "react-router-dom";
 
-import Nav from "../../Nav/Nav";
-import AboutHero from "../../AboutHero/AboutHero";
-import AsgaardSofa from "../../AsgaardSofa/AsgaardSofa";
-import DescripSec from "../../DescripSec/DescripSec";
-import RelatedProducts from "../../RelatedProducts/RelatedProducts";
-import FooterSec from "../../FooterSec/FooterSec";
-import Footer from "../../Footer/Footer";
+// import Nav from "../../Nav/Nav";
+// import AboutHero from "../../AboutHero/AboutHero";
+// import AsgaardSofa from "../../AsgaardSofa/AsgaardSofa";
+// import DescripSec from "../../DescripSec/DescripSec";
+// import RelatedProducts from "../../RelatedProducts/RelatedProducts";
+// import FooterSec from "../../FooterSec/FooterSec";
+// import Footer from "../../Footer/Footer";
+
+// const Details = () => {
+
+//   const { id } = useParams();
+
+//   return (
+//     <div>
+
+//       <Nav />
+//       <AsgaardSofa id={id} />
+//       <DescripSec />
+//       <RelatedProducts />
+//       <FooterSec />
+//       <Footer marginTop="mt-1" />
+
+//     </div>
+//   );
+// };
+
+// export default Details;
+
+
+import React from "react";
 
 const Details = () => {
-
-  const { id } = useParams();
-
+  console.log("NEW DETAILS CODE 123");
   return (
-    <div>
-
-      <Nav />
-      <AsgaardSofa id={id} />
-      <DescripSec />
-      <RelatedProducts />
-      <FooterSec />
-      <Footer marginTop="mt-1" />
-
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-red-500">
+        DETAILS PAGE TEST
+      </h1>
     </div>
   );
 };
